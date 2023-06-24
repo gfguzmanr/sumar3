fee508ef095bb08f0054336516d6ee672739b631
LAB_INFORMATICA <guzfamilia@hotmail.com>
2023-06-24 01:17:55 -0500

    v.1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+package co.open_bootcamp;
+public class Main {
+    public static void main(String[] args) {
+        int total = 0;
+        total = sumar3(5,7,10);
+        System.out.println(total);
+        }
+         public static int sumar3(int a, int b, int c){
+                return a+b+c;
+         }
+    }
